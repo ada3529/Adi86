ed85024bd63a2897279efa8d8f2d592f4a7e3031
ada3529 <98525897+ada3529@users.noreply.github.com>
2022-06-21 19:09:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 //=============『 Utama 』================== //
-global.owner = ['6287892711054'] 
+global.owner = ['6283188622570'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Hyzer Official'
-global.numberowner = '6287892711054'
-global.instagram = 'https://instagram.com/hyzer_stfu'
-global.github = 'https://github.com/Hyzerr'
-global.dana = '087892711054'
-global.pulsa = '085212023766'
-global.gopay = '087892711054'
+global.nameowner = 'Test'
+global.numberowner = '6283188622570'
+global.instagram = 'https://instagram.com/firmanhes78_'
+global.github = 'https://github.com/ada3529'
+global.dana = '083188622570'
+global.pulsa = '083188622570'
 
 //=============『 Info Bot 』=================//
 global.namebot = 'ShiroBot-Md'
-global.gc = 'https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV'
+global.gc = 'https://chat.whatsapp.com/HrGKrmcJFNdC4AoH5hl6Lp'
 global.web = 'https://hyzerr-apii.herokuapp.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
